5a9dc4dee50217bd06eaeeef3b19d50496959dc7
Babali <bab07ali@gmail.com>
2020-05-10 15:33:55 +0200

    add mouse interaction

--- src/app/map-component/map-component.component.ts
+++ src/app/map-component/map-component.component.ts
@@ -7,6 +7,9 @@ import Icon from 'ol/style/Icon';
 import OSM from 'ol/source/OSM';
 import * as olProj from 'ol/proj';
 import TileLayer from 'ol/layer/Tile';
+import { defaults as defaultControls, OverviewMap } from 'ol/control';
+import * as olCoordinates from 'ol/coordinate';
+import MousePosition from 'ol/control/mouseposition';
 
 @Component({
   selector: 'app-map-component',
@@ -18,10 +21,29 @@ export class MapComponentComponent implements OnInit {
   constructor() { }
 
   private map;
+  private mousePosition;
+  private overviewMapControl;
+  private source;
 
   ngOnInit(): void {
+    console.log("INIT");
+
+    this.source = new OSM();
+    this.overviewMapControl = new OverviewMap({
+      layers: [
+        new TileLayer({
+          source: this.source
+        })
+      ]
+    });
+
+
+
     this.map = new Map({
       target: 'hotel_map',
+      controls: defaultControls().extend([
+        this.overviewMapControl
+      ]),
       layers: [
         new TileLayer({
           source: new OSM()
@@ -30,8 +52,25 @@ export class MapComponentComponent implements OnInit {
       view: new View({
         center: olProj.fromLonLat([5.9913, 45.1709]),
         zoom: 5
-      })
+      }),
     });
+    
+
+    this.mousePosition = new MousePosition({
+      coordinateFormat: olCoordinates.createStringXY(2),
+      projection: 'EPSG:4326',
+      target: document.getElementById('myposition'),
+      undefinedHTML: '&nbsp;'
+    });
+  
+    this.map.addControl(this.mousePosition);
   }
+
+  clickMap(event:any):void{
+    console.log(event);
+    console.log(this.mousePosition);
+  }
+
+  
 
 }

4dae5704de1de1a969e1232e3f3154ad1834a627
Baptiste LYET <baptiste.lyet@agixis.com>
2021-01-11 23:48:55 +0100

    delete mongodb

--- server.js
+++ server.js
@@ -14,100 +14,100 @@ app.use(bodyParser.json());
 var db;
 
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", {useUnifiedTopology: true}, function (err, client) {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    }
+// mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", {useUnifiedTopology: true}, function (err, client) {
+//     if (err) {
+//         console.log(err);
+//         process.exit(1);
+//     }
 
-    // Save database object from the callback for reuse.
-    db = client.db();
-    console.log("Database connection ready");
+//     // Save database object from the callback for reuse.
+//     db = client.db();
+//     console.log("Database connection ready");
 
     // Initialize the app.
     var server = app.listen(process.env.PORT || 8080, function () {
         var port = server.address().port;
         console.log("App now running on port", port);
     });
-});
+// });
 
-// PLACES API ROUTES BELOW
+// // PLACES API ROUTES BELOW
 
-// Generic error handler used by all endpoints.
-    function handleError(res, reason, message, code) {
-        console.log("ERROR: " + reason);
-        res.status(code || 500).json({"error": message});
-    }
+// // Generic error handler used by all endpoints.
+//     function handleError(res, reason, message, code) {
+//         console.log("ERROR: " + reason);
+//         res.status(code || 500).json({"error": message});
+//     }
   
-    /*  "/api/places"
-    *    GET: finds all places
-    *    POST: creates a new place
-    */
+//     /*  "/api/places"
+//     *    GET: finds all places
+//     *    POST: creates a new place
+//     */
 
-    app.get("/api/places", function(req, res) {
-        db.collection(PLACES_COLLECTION).find({}).toArray(function(err, docs) {
-        if (err) {
-            handleError(res, err.message, "Failed to get places.");
-        } else {
-            res.status(200).json(docs);
-        }
-        });
-    });
+//     app.get("/api/places", function(req, res) {
+//         db.collection(PLACES_COLLECTION).find({}).toArray(function(err, docs) {
+//         if (err) {
+//             handleError(res, err.message, "Failed to get places.");
+//         } else {
+//             res.status(200).json(docs);
+//         }
+//         });
+//     });
     
-    app.post("/api/places", function(req, res) {
-        var newPlace = req.body;
-        newPlace.createDate = new Date();
+//     app.post("/api/places", function(req, res) {
+//         var newPlace = req.body;
+//         newPlace.createDate = new Date();
     
-        if (!req.body.name) {
-        handleError(res, "Invalid user input", "Must provide a name.", 400);
-        } else {
-        db.collection(PLACES_COLLECTION).insertOne(newPlace, function(err, doc) {
-            if (err) {
-            handleError(res, err.message, "Failed to create new place.");
-            } else {
-            res.status(201).json(doc.ops[0]);
-            }
-        });
-        }
-    });
+//         if (!req.body.name) {
+//         handleError(res, "Invalid user input", "Must provide a name.", 400);
+//         } else {
+//         db.collection(PLACES_COLLECTION).insertOne(newPlace, function(err, doc) {
+//             if (err) {
+//             handleError(res, err.message, "Failed to create new place.");
+//             } else {
+//             res.status(201).json(doc.ops[0]);
+//             }
+//         });
+//         }
+//     });
   
   
-  /*  "/api/places/:id"
-   *    GET: find place by id
-   *    PUT: update place by id
-   *    DELETE: deletes place by id
-   */
+//   /*  "/api/places/:id"
+//    *    GET: find place by id
+//    *    PUT: update place by id
+//    *    DELETE: deletes place by id
+//    */
   
-  app.get("/api/places/:id", function(req, res) {
-    db.collection(PLACES_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
-      if (err) {
-        handleError(res, err.message, "Failed to get place");
-      } else {
-        res.status(200).json(doc);
-      }
-    });
-  });
+//   app.get("/api/places/:id", function(req, res) {
+//     db.collection(PLACES_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+//       if (err) {
+//         handleError(res, err.message, "Failed to get place");
+//       } else {
+//         res.status(200).json(doc);
+//       }
+//     });
+//   });
   
-  app.put("/api/places/:id", function(req, res) {
-    var updateDoc = req.body;
-    delete updateDoc._id;
+//   app.put("/api/places/:id", function(req, res) {
+//     var updateDoc = req.body;
+//     delete updateDoc._id;
   
-    db.collection(PLACES_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
-      if (err) {
-        handleError(res, err.message, "Failed to update place");
-      } else {
-        updateDoc._id = req.params.id;
-        res.status(200).json(updateDoc);
-      }
-    });
-  });
+//     db.collection(PLACES_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+//       if (err) {
+//         handleError(res, err.message, "Failed to update place");
+//       } else {
+//         updateDoc._id = req.params.id;
+//         res.status(200).json(updateDoc);
+//       }
+//     });
+//   });
   
-  app.delete("/api/places/:id", function(req, res) {
-    db.collection(PLACES_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
-      if (err) {
-        handleError(res, err.message, "Failed to delete place");
-      } else {
-        res.status(200).json(req.params.id);
-      }
-    });
-  });
+//   app.delete("/api/places/:id", function(req, res) {
+//     db.collection(PLACES_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+//       if (err) {
+//         handleError(res, err.message, "Failed to delete place");
+//       } else {
+//         res.status(200).json(req.params.id);
+//       }
+//     });
+//   });
